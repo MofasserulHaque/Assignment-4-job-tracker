c914317e50cb75d0e202851c506bf78aeaaed7e6
MofasserulHaque <mofasserulhaque11@gmail.com>
2026-03-06 09:25:05 +0600

    trying to code java

---
+++ js/JS-intervew.js
@@ -0,0 +1,9 @@
+const btnContainer= document.getElementsByClassName(' interview-btn ');
+for(containerBtn of btnContainer){
+    containerBtn.addEventListener('click',function(){
+        
+    const creatingSection=  document.createElement('section');
+    
+        
+    })
+}
